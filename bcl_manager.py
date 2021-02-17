a0cdbe2253e1927fe6d6c26f8a6515fa9e746f7e
Aaron Fishman <uk.afishman@gmail.com>
2021-02-17 15:46:00 +0000

    check for CopyComplete.txt

--- bcl_manager.py
+++ bcl_manager.py
@@ -1,8 +1,37 @@
 import sys
 import time
 import logging
+import ntpath
+
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import LoggingEventHandler, FileCreatedEvent, FileSystemEventHandler
+
+
+class IlluminaPlateTransferredEvent(FileSystemEventHandler):
+    """File system event representing file creation on the file system."""
+
+    def __init__(self, copy_complete_filename='CopyComplete.txt'):
+        super(IlluminaPlateTransferredEvent, self).__init__()
+
+        self.copy_complete_filename = copy_complete_filename
+
+    def on_created(self, event):
+        """Called when a file or directory is created.
+
+        :param event:
+            Event representing file/directory creation.
+        :type event:
+            :class:`DirCreatedEvent` or :class:`FileCreatedEvent`
+        """
+
+        print('event: ', event)
+
+        # Check the file
+        if (ntpath.basename(event.src_path) != self.copy_complete_filename):
+            return
+
+        print('New Illumina Plate Transferred')
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
@@ -10,6 +39,9 @@ if __name__ == "__main__":
                         datefmt='%Y-%m-%d %H:%M:%S')
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
     event_handler = LoggingEventHandler()
+    event_handler = IlluminaPlateTransferredEvent()
+
+
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
