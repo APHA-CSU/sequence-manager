fac5dc34b803ad7b769d147378d5a97c8ea5edd5
Aaron Fishman <uk.afishman@gmail.com>
2021-03-22 15:30:04 +0000

    comment

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ s3 = boto3.resource('s3')
 
 def s3_object_exists(bucket, key):
     """
-        Returns true if the S3 key in the S3 bucket. False otherwise
+        Returns true if the S3 key is in the S3 bucket. False otherwise
         Thanks: https://stackoverflow.com/questions/33842944/check-if-a-key-exists-in-a-bucket-in-s3-using-boto3
     """
     
@@ -28,11 +28,11 @@ def s3_object_exists(bucket, key):
 
 def s3_sync(src_dir, bucket, key):
     """
-        Synchronise src_dir to s3://{bucket}/{key}
+        Upload src_dir to s3://{bucket}/{key}
     """
     target_uri = f's3://{bucket}/{key}'
 
-    # Make sure the key exists 
+    # Don't overwrite 
     if s3_object_exists(bucket, key):
         raise Exception(f'{target_uri} already exists')
 
