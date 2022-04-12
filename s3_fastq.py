55592fa45810a629005e714946b11b8fd901fca7
Aaron Fishman <uk.afishman@gmail.com>
2022-04-12 15:06:48 +0100

    removing old code

--- s3_fastq.py
+++ s3_fastq.py
@@ -4,39 +4,6 @@ import subprocess
 
 import re
 
-def boto():
-    bucket_name = 's3-csu-001'
-    project_codes = ['SB4030']
-
-    s3_resource = boto3.resource('s3')
-    bucket = s3_resource.Bucket(bucket_name)
-
-    project_code = project_codes[0]
-
-    objects = bucket.objects.filter(Prefix=project_code + '/', Delimiter='/')
-
-    summaries = []
-    for obj in objects:
-        object_type = "directory" if obj.key.endswith('/') else 'file'
-
-        summary = {
-            "key": obj.key,
-            "type": object_type,
-            "upload_date": str(obj.meta.__dict__['data']['LastModified'])
-        }
-        summaries.append(summary)
-
-    df = pd.DataFrame(summaries)
-    df.to_csv('./summary.csv')
-
-
-def ls(uri):
-    a = subprocess.run(["aws", "s3", "ls", uri], capture_output=True, check=True, text=True)
-    lines = a.stdout.split('\n')
-
-    files = []
-
-    b = 2
 
 def list_keys(bucket_name='s3-csu-001', prefix='SB4030/M02410_5267/'):
     s3 = boto3.resource('s3')
@@ -46,20 +13,6 @@ def list_keys(bucket_name='s3-csu-001', prefix='SB4030/M02410_5267/'):
     objects = bucket.objects.filter(Prefix=prefix)
 
     return [obj.key for obj in objects]
-
-    for obj in objects:
-        summaries = []
-        for obj in objects:
-            object_type = "directory" if obj.key.endswith('/') else 'file'
-
-            summary = {
-                "key": obj.key,
-                "type": object_type,
-                "upload_date": str(obj.meta.__dict__['data']['LastModified'])
-            }
-            summaries.append(summary)
-
-    return pd.DataFrame(summaries)
 
 def pair_files(keys):
     """
@@ -151,12 +104,6 @@ def pair_files(keys):
         not_parsed        
     )
 
-def list_subfolders(bucket='s3-csu-001', prefix='SB4030'):    
-    client = boto3.client('s3')
-    result = client.list_objects(Bucket=bucket, Prefix=prefix+'/', Delimiter='/')
-
-    return [o.get('Prefix') for o in result.get('CommonPrefixes')]
-
 def plate_summary(samples):
     df = samples.groupby("id")
 
@@ -206,8 +153,8 @@ def list_tb_samples():
     a = 1
 
 
-# keys = pd.read_csv('s3-csu-001_keys.csv')["0"].to_list()
-# a = pair_files(keys)
+keys = pd.read_csv('s3-csu-001_keys.csv')["0"].to_list()
+a,b,c,d = pair_files(keys)
 # b = 1
 
 
