e87061ce3416062c1c50a0b3fce8b9f50cd29fb6
Aaron Fishman <uk.afishman@gmail.com>
2022-04-13 18:32:40 +0100

    comments

--- install.bash
+++ install.bash
@@ -1,9 +1,20 @@
 set -eo pipefail
 
+#================================================================
+# install.bash
+#================================================================
+#% DESCRIPTION
+#%    Install dependancies for a job for the TB reprocess
+#%
+#% INPUTS
+#%    job_id        id of the job
+
 # apt
 sudo apt-get update
 sudo apt-get -y install python3 python3-pip docker.io systemctl
 
+# TODO: install docker
+
 # python 
-sudo pip3 install biopython numpy pandas gitpython
+sudo pip3 install biopython numpy pandas gitpython boto3
 
