5fff72e5bee3e800d5acb329542ba2204831aa99
Aaron Fishman <uk.afishman@gmail.com>
2022-04-13 18:26:39 +0100

    logs

--- launch.py
+++ launch.py
@@ -1,10 +1,13 @@
 import argparse
 import sys
 import subprocess
-from matplotlib.pyplot import subplot2grid
 import os
+import tempfile
+import logging
 
 import pandas as pd
+
+from s3_logging_handler import S3LoggingHandler
 
 # TODO: set image to prod
 DEFAULT_IMAGE = "aaronsfishman/bov-tb:master"
@@ -12,22 +15,33 @@ DEFAULT_IMAGE = "aaronsfishman/bov-tb:master"
 # TODO: update uris
 DEFAULT_ENDPOINT = "s3://s3-staging-area/AaronFishman/"
 DEFAULT_PLATES_URI = "s3://s3-staging-area/AaronFishman/plates.csv"
+LOGGING_BUCKET = "s3-staging-area"
+LOGGING_PREFIX = "AaronFishman/logs/"
 
 def launch(job_id, endpoint=DEFAULT_ENDPOINT, plates_uri=DEFAULT_PLATES_URI):
     """ Launches a job for a specific EC2 instance """
 
-    # Select plates
+    # Download plates csv from S3
+    logging.info(f"Downloading plates csv from {plates_uri}")
     subprocess.run(["aws", "s3", "cp", plates_uri, "./plates.csv"])
     plates = pd.read_csv('./plates.csv')
-    plates = plates.loc[plates.job_id==job_id, :]
+    plates = plates.loc[plates.job_id==job_id, :].reset_index(level=0)
 
     # Process one plate at a time
     for i, plate in plates.iterrows():
+        logging.info(f"""
+            Running plate {i+1}/{len(plates)}
+                bucket: {plate["bucket"]}
+                prefix: {plate["prefix"]}
+        """)
         reads_uri = f's3://{plate["bucket"]}/{plate["prefix"]}'
         results_uri = f'{endpoint}/{plate["prefix"]}/'
         
-        run_pipeline_s3(reads_uri, results_uri)        
+        try:
+            run_pipeline_s3(reads_uri, results_uri)
 
+        except Exception as e:
+            logging.exception(e)
 
 def run_pipeline_s3(reads_uri, results_uri, image=DEFAULT_IMAGE):
     """ Run pipeline from S3 uris """
@@ -39,22 +53,23 @@ def run_pipeline_s3(reads_uri, results_uri, image=DEFAULT_IMAGE):
     if not results_uri.startswith("s3://"):
         raise Exception(f"Invalid results uri: {results_uri}")
     
-    # Make temp folder
-    # TODO: use python temp folder to ensure it is removed
-    temp_reads = f"./{reads_uri[5:]}/reads/"
-    temp_results = f"./{reads_uri[5:]}/results/"
+    # Temp directory ensures we don't get lots of data accumulating
+    with tempfile.TemporaryDirectory() as temp_dirname:
+        # Make I/O Directories
+        temp_reads = f"{temp_dirname}/reads/"
+        temp_results = f"{temp_dirname}/results/"
 
-    os.makedirs(temp_reads)
-    os.makedirs(temp_results)
+        os.makedirs(temp_reads)
+        os.makedirs(temp_results)
 
-    # Download
-    subprocess.run(["aws", "s3", "cp", "--recursive", reads_uri, temp_reads], check=True)
+        # Download
+        subprocess.run(["aws", "s3", "cp", "--recursive", reads_uri, temp_reads], check=True)
 
-    # Run
-    run_pipeline(temp_reads, temp_results)
+        # Run
+        run_pipeline(temp_reads, temp_results)
 
-    # Upload
-    subprocess.run(["aws", "s3", "cp", "--recursive", temp_results, results_uri], check=True)
+        # Upload
+        subprocess.run(["aws", "s3", "cp", "--recursive", temp_results, results_uri], check=True)
 
 def run_pipeline(reads, results, image=DEFAULT_IMAGE):
     """ Run the pipeline using docker """
@@ -79,6 +94,19 @@ def main(args):
     parser.add_argument("job", help="Job ID")
 
     parsed = parser.parse_args(args)
+
+    # Setup logging
+    log_prefix = f"{LOGGING_PREFIX}/{parsed.job}"
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        handlers=[
+            logging.StreamHandler(),
+            S3LoggingHandler(f'./{parsed.job}.log', LOGGING_BUCKET, log_prefix)
+        ]
+    )
 
     # Run
     launch(parsed.job)
